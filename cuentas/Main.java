2f0b9e3221365f04c45e24181b211745aa1c77bb
Edertg <a24edertg@iessanclemente.net>
2025-03-02 18:55:25 +0100

    Renombrado miCuenta a cuenta1 en Main.java y agregado símbolo €

--- cuentas/Main.java
+++ cuentas/Main.java
@@ -1,22 +1,26 @@
 package cuentas;
+
+/**
+ * Clase que gestiona la operativa de la cuenta.
+ */
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;	// sustituimos micuenta por cuenta1
         double saldoActual;
 
-        miCuenta = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);	// sustituimos micuenta por cuenta1
+        saldoActual = cuenta1.estado();	// sustituimos micuenta por cuenta1
+        System.out.println("El saldo actual es "+ saldoActual + "€");	// añadimos un espacio despues de "es" para que se separe del saldo y añadimos el símbolo "€".
 
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(2300); // sustituimos micuenta por cuenta1
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+           cuenta1.ingresar(695);	// sustituimos micuenta por cuenta1
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
