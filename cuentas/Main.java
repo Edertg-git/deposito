0df58f77cf3961f00393cfc4bc3a96f9cdcf67f9
Edertg <a24edertg@iessanclemente.net>
2025-03-02 19:02:45 +0100

    Introducimos el método operativa_cuenta, que engloba las sentencias de la clase Main que operan con el objeto cuenta1

--- cuentas/Main.java
+++ cuentas/Main.java
@@ -6,23 +6,33 @@ package cuentas;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta cuenta1;	// sustituimos micuenta por cuenta1
+        operativa_cuenta(695.0f); // Llamamos a operativa_cuenta con una cantidad de ingreso
+    }
+
+    /**
+     * Método que realiza operaciones sobre una cuenta bancaria.
+     * 
+     * @param cantidad Cantidad de dinero a ingresar en la cuenta.
+     */
+    public static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1; // sustituimos micuenta por cuenta1
         double saldoActual;
 
-        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);	// sustituimos micuenta por cuenta1
-        saldoActual = cuenta1.estado();	// sustituimos micuenta por cuenta1
-        System.out.println("El saldo actual es "+ saldoActual + "€");	// añadimos un espacio despues de "es" para que se separe del saldo y añadimos el símbolo "€".
+        cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es " + saldoActual + "€");
 
         try {
-            cuenta1.retirar(2300); // sustituimos micuenta por cuenta1
+            cuenta1.retirar(2300);
         } catch (Exception e) {
-            System.out.print("Fallo al retirar");
+            System.out.println("Fallo al retirar");
         }
+
         try {
             System.out.println("Ingreso en cuenta");
-           cuenta1.ingresar(695);	// sustituimos micuenta por cuenta1
+            cuenta1.ingresar(cantidad); // Se usa la cantidad recibida por parámetro
         } catch (Exception e) {
-            System.out.print("Fallo al ingresar");
+            System.out.println("Fallo al ingresar");
         }
     }
 }
